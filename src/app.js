1f3f4e737118ca0eaa47c32e45380ea3095926f4
RushDynamic <tameimpalafanclubpresident@gmail.com>
2022-03-11 22:49:58 +0530

    clean: switched to iife

--- src/app.js
+++ src/app.js
@@ -1,14 +1,9 @@
 import question from './utils/rl.js';
 import monitor from './services/monitoring-service.js';
 
-let coinsList = [];
-
-const getCoins = () => {
+(() => {
 	console.clear();
 	question('Enter a list of Coin Ticker Symbols: ', (resp) => {
-		coinsList = resp.split(' ');
-		monitor(coinsList);
+		monitor(resp.split(' '));
 	});
-};
-
-getCoins();
+})();
